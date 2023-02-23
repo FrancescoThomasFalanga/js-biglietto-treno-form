994e39c19394cfa1a1a0d96f231f600eee4a2c50
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-02-23 12:57:08 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del
+passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
+seguendo delle determinate regole.
+*/
+
+/*
+x Chiedi all'utente il numero di chilometri che vuole percorrere e memorizzali
+x Chiedi all'utente l'età del passeggero e memorizzala
+x Il prezzo del biglietto è definito in base ai km (0.21 $ al km)
+? SE il passeggero è minorenne
+    applica uno sconto del 20%
+:? ALTRIMENTI SE il passeggero ha un'età compresa tra i 18 e i 64 anni
+    non applicare nessuno sconto
+: ALTRIMENTI applica uno sconto del 40%
+*/
+
+/*
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone 
+(non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale 
+(o output) sarà anch’essa da scrivere in console.
+
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui 
+l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina 
+(il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). 
+Questo richiederà un minimo di ricerca.
+*/
